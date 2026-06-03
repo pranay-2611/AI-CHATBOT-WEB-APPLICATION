abe45c854cdef028ff47202f695c1145e6531f67
pranay-2611 <pranaykarumuri3@gmail.com>
2026-06-03 13:44:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import streamlit as st
 import google.generativeai as genai
 from dotenv import load_dotenv
 
-load_dotenv()
 
 api_key = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=api_key)
